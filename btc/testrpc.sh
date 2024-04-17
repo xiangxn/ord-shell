61c0f40fbceabee0349c23f8c614808d15e42d80
necklace <xiangxn@163.com>
2024-04-17 11:51:02 +0800

    update

--- btc/testrpc.sh
+++ btc/testrpc.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 curl -s -X POST --user btc:btc2024  \
-  -H 'content-type: text/plain;' http://127.0.0.1:18332/ \
+  -H 'content-type: text/plain;' http://127.0.0.1:18333/ \
   --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getmininginfo", "params": [] }'
